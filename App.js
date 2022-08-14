8d6a43043c29d5dbd75b1e0dcb181097e6279429
Anderson <cruz.andersom@gmail.com>
2022-08-14 19:40:02 -0300

    aula_01_criacao_da_aplicacao

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text> Hoje vai ter uma festa;
+        bolo e garana e muito doce pra comer</Text>
+
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
